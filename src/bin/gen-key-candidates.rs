1da54dff740b0455cca55ef4ef136938434c930b
Marclie <marclie1@gmx.de>
2024-11-09 00:18:32 +0100

    optimization + final

--- src/bin/gen-key-candidates.rs
+++ src/bin/gen-key-candidates.rs
@@ -8,24 +8,24 @@ use rand::{Rng, SeedableRng};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 
-
+const SECOND_FRACTION: u128 = 1_000;
 
 #[tokio::main]
 async fn main() {
+    // Get number of days from command line
+    let days = std::env::args().nth(1).expect("Usage: gen_key_candidates <hours> <?start_unix_time>");
+    let days = days.parse::<u64>().unwrap();
     // Get start time in unix seconds from command line
     let start_time = std::env::args().nth(2).unwrap_or(format!("{}", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()));
     let start_time = start_time.parse::<u64>().unwrap();
-    // Get number of days from command line
-    let days = std::env::args().nth(1).expect("Usage: gen_key_candidates <days> <start_time>");
-    let days = days.parse::<u64>().unwrap();
     // Get the end time in unix seconds
-    let end_time = start_time - (days * 86400);
+    let end_time = start_time - (days * 60 * 60);
 
-    let end_seed = start_time as u128 * 1000;
-    let start_seed = end_time as u128 * 1000;
+    let end_seed = start_time as u128 * SECOND_FRACTION;
+    let start_seed = end_time as u128 * SECOND_FRACTION;
     println!("Generating keys from {} to {}", start_seed, end_seed);
-    let seeds = (start_seed..end_seed).map(|x| x as u64).collect::<Vec<u64>>();
-    println!("Generating {} keys", seeds.len());
+    let seeds = (start_seed..end_seed).rev().map(|x| x as u64).collect::<Vec<u64>>();
+    // println!("Generating {} keys", seeds.len());
 
     let file = OpenOptions::new()
         .write(true)
@@ -70,17 +70,20 @@ async fn main() {
         handels.push(handle);
     }
 
-
-    seeds.into_par_iter().for_each(|seed| {
-        let mut rng = rand_chacha::ChaChaRng::seed_from_u64(seed);
-        let key: [u8; 32] = rng.gen();
-        let hex_string: String = format!("{:02X?}", key).replace(", ", "").replace("[", "").replace("]", "");
-        let tx = tx.clone();
-        if hex_string == "EFC800EAF150870461D5AE942423D332AB9B044DE25D049C43392EEB66163071" {
-            println!("Seed: {}", seed);
+    for (i, chunk) in seeds.chunks(1 << 24).enumerate() {
+        println!("Calculating keys for chunk {}", i);
+        chunk.into_par_iter().for_each_with(tx.clone(), |tx_local, seed| {
+            let mut rng = rand_chacha::ChaChaRng::seed_from_u64(*seed);
+            let key: [u8; 32] = rng.gen();
+            let hex_string: String = format!("{:02X?}", key).replace(", ", "").replace("[", "").replace("]", "");
+            tx_local.send(hex_string).unwrap();
+        });
+        // Sleep for a second to allow the other threads to write to the file
+        while tx.len() > 0 {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
         }
-        tx.send(hex_string).unwrap();
-    });
+    }
+
     println!("Done calculating keys");
 
     while tx.len() > 0 {
