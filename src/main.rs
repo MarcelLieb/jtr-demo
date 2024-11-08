42766867326971dfd02218b98a2047747a74e4c1
Marclie <marclie1@gmx.de>
2024-11-08 18:17:34 +0100

    final demo candidate

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
+use std::{fs::File, io::Read};
+
 use md5::{Digest, Md5};
 
-static SECRET: &str = "db49f02a426bf605bd8a708500d6dcfa";
+// digest of the hashes.txt file
+static SECRET: &str = "";
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let input = std::env::args().nth(1).expect("Usage: demo <key>");
+    let mut file = String::new();
+    let _ = File::open("hashes.txt")?.read_to_string(&mut file)?;
+    let hashes = file.lines().map(|s| s.to_string()).collect::<Vec<String>>();
+
 
     let mut hasher = Md5::new();
+    hasher.update(file);
+    let verification = hasher.finalize();
+    let verification = format!("{:x}", verification);
 
-
+    if !SECRET.is_empty() && verification != SECRET {
+        println!("Invalid key list");
+        return Ok(());
+    }
+    
+    // Hash input
+    let mut hasher = Md5::new();
     hasher.update(input);
-
     let input = hasher.finalize();
 
-    if format!("{:x}", input) != SECRET {
+    if !hashes.contains(&format!("{:x}", input)) {
+        println!("{:x}", input);
         println!("Invalid key");
-        return;
+        return Ok(());
     }
 
     println!("Congratulations! The flag is: {}", std::env::var("FLAG").unwrap_or("FLAG not set".to_string()));
+    Ok(())
 }
